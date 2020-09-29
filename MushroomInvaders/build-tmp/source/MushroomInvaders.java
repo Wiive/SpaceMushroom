b4d17474049f94979cfcc6bdf96f3491b2544e62
Wiive <wirrechan@hotmail.com>
2020-09-29 11:55:18 +0200

    Created Enemy class and updated Mainfile

--- MushroomInvaders/build-tmp/source/MushroomInvaders.java
+++ MushroomInvaders/build-tmp/source/MushroomInvaders.java
@@ -15,6 +15,8 @@ import java.io.IOException;
 public class MushroomInvaders extends PApplet {
 
 Player player;
+Enemy[] enemies;
+int numberOfEnemys = 5;
 float deltaTime;
 long time;
 
@@ -24,6 +26,11 @@ public void setup()
 	frameRate(60);
 
 	player = new Player(400,850,30,200,100,150);
+	enemies = new Enemy[numberOfEnemys];
+	for (int i = 0; i < numberOfEnemys; ++i)
+	{
+		enemies[i] = new Enemy(50 + (i*50), 50, 30,1);
+	}
 }
 
 public void draw()
@@ -37,7 +44,76 @@ public void draw()
 	player.update();
 	player.draw();
 
+	for(int i = 0; i < enemies.length; ++i)
+	{
+		enemies[i].update();
+		enemies[i].draw();
+	}
+
+
 	time = currentTime;
+}
+class Enemy extends GameObject
+{
+	float horizontalSpeed = 20;
+	float verticalSpeed = 20;
+	boolean hitScreenWall;
+
+	int scoreValue;
+	int scoreTier1 = 10;
+	int scoreTier2 = 20;
+	int scoreTier3 = 30;
+	int colorTier1 = color(0, 200, 0);
+	int colorTier2 = color(200, 0, 0);
+	int colorTier3 = color(0, 0, 200);
+
+	Enemy(float x, float y, int size, int tierType)
+	{
+		super(x,y,size);
+		position.x = x;
+		position.y = y;
+		this.size = size;
+
+		if (tierType == 1)
+		{
+			scoreValue = scoreTier1;
+			objectColor = colorTier1;
+		}
+		else if(tierType == 2)
+		{
+			scoreValue = scoreTier2;
+			objectColor = colorTier2;
+		}
+		else if(tierType == 3)
+		{
+			scoreValue = scoreTier3;
+			objectColor = colorTier3;
+		}
+	}
+
+	public void update()
+	{
+		position.x = position.x + horizontalSpeed * deltaTime;
+		if(position.x < size || position.x > width - size)
+		hitScreenWall = true;
+		if(hitScreenWall)
+		{
+			print("Hitted wall");
+			horizontalSpeed = horizontalSpeed * -1;
+			position.y = position.y + verticalSpeed * deltaTime;
+			hitScreenWall = false;
+		}
+
+	
+	}
+
+	public void draw()
+	{
+		fill(objectColor);
+		ellipse(position.x, position.y, size, size);
+	}
+
+
 }
 class GameObject
 {
